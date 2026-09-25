99dea3270ca6e60e35165691638017a4cae2ea56
NXGAME-ADM <nxgamegiftpack@gmail.com>
2026-09-25 17:44:22 +0800

    Update promo.ts

--- src/promo.ts
+++ src/promo.ts
@@ -7,10 +7,10 @@ export const EARLY_PROMO_CODE = 'WEEKEND10'
 export const EARLY_PROMO_DISCOUNT_PERCENT = 10
 
 /*
- * Sunday, September 20, 2026 at 11:59:59 PM Singapore / GMT+8.
- * UTC equivalent: September 20, 2026 at 15:59:59.999.
+ * Sunday, September 27, 2026 at 11:59:59 PM Singapore / GMT+8.
+ * new UTC equivalent: September 27, 2026 at 15:59:59.999.
  */
-export const EARLY_PROMO_END_ISO = '2026-09-20T15:59:59.999Z'
+export const EARLY_PROMO_END_ISO = '2026-09-27T15:59:59.999Z'
 
 export interface PromoApplyResult {
   success: boolean
